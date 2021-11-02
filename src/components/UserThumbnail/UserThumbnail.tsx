bb89f55b264f1695eb19f4ffe390bd8bc3f7b852
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-11-02 11:56:49 +0000

    search by username working

--- src/components/UserThumbnail/UserThumbnail.tsx
+++ src/components/UserThumbnail/UserThumbnail.tsx
@@ -14,7 +14,7 @@ const UserThumbnail = ({ user }: Props) => {
             <div className="user-thumbnail d-flex py-3">
                 <img className="rounded-circle" height={50} width={50} src={user.picture.thumbnail} alt="user-img" />
                 <div className="d-flex flex-column align-items-start px-3">
-                    <h6 className="mb-0">{user.name.title + user.name.first + user.name.last}</h6>
+                    <h6 className="mb-0">{user.name.title + " " + user.name.first + " " + user.name.last}</h6>
                     <Link to={`/${user.login.username}`} >
                         {user.login.username}
                     </Link>
