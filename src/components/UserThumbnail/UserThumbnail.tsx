9cadb1387adc76878f408f027a5eca1b7ba26592
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-11-02 12:22:47 +0000

    user thumbnail styling

--- src/components/UserThumbnail/UserThumbnail.tsx
+++ src/components/UserThumbnail/UserThumbnail.tsx
@@ -11,17 +11,17 @@ const UserThumbnail = ({ user }: Props) => {
 
     return (
         <Col md={6}>
-            <div className="user-thumbnail d-flex py-3">
-                <img className="rounded-circle" height={50} width={50} src={user.picture.thumbnail} alt="user-img" />
-                <div className="d-flex flex-column align-items-start px-3">
-                    <h6 className="mb-0">{user.name.title + " " + user.name.first + " " + user.name.last}</h6>
-                    <Link to={`/${user.login.username}`} >
+            <Link  className="no-decoration" to={`/${user.login.username}`} >
+                <div className="user-thumbnail d-flex p-3 my-2">
+                    <img className="rounded-circle" height={50} width={50} src={user.picture.thumbnail} alt="user-img" />
+                    <div className="d-flex flex-column align-items-start px-3">
+                        <h6 className="mb-0">{user.name.title + " " + user.name.first + " " + user.name.last}</h6>
                         {user.login.username}
-                    </Link>
-                    <span className="text-muted">{user.email}</span>
-                </div>
+                        <a href={`mailto:${user.email}`} target="_blank" rel="noreferrer" >{user.email}</a>
+                    </div>
 
-            </div>
+                </div>
+            </Link>
         </Col >
     )
 }
